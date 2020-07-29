50535515409727d27f96c974140875e25d901112
Okorie Chinedu Sunday <okoriechinedusunday@gmail.com>
2020-07-29 10:07:45 +0100

    Update Register.js
    remove key

--- src/components/sign-up/Register.js
+++ src/components/sign-up/Register.js
@@ -213,7 +213,7 @@ const Register = ({
 							<div>
 								<GoogleLogin
 									clientId={
-										'658977310896-knrl3gka66fldh83dao2rhgbblmd4un9.apps.googleusercontent.com'
+										'xxxx'
 									}
 									onSuccess={responseFromGoogle}
 									onFailure={responseFromGoogle}
